608fbd1046d8b642fb6c9cf3d6cf867877fc1c38
timothe <timothe@tim>
2018-03-26 15:26:16 +0200

    mise a jour de la methode validEnvoie

--- public_html/JavaScript/myScript.js
+++ public_html/JavaScript/myScript.js
@@ -1,13 +1,11 @@
 function validEnvoi() {
-    if (
-            window.document.querySelector("#i_nom").value==="" &&window.document.querySelector("#i_prenom").value==="") {
-            alert("Le nom ou le prénom doivent être remplis");// On affiche un message
-        }
-        else {
-            window.document.querySelector("#form_contact").submit();// On peut envoyer
-        }
+    if (window.document.querySelector("#i_nom").value==="" && window.document.querySelector("#i_prenom").value==="") {
+        alert("Le nom ou le prénom doivent être remplis");// On affiche un message
     }
-    window.addEventListener("load", function() {
-        window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
-    }
-);
+    else if (window.document.querySelector("#i_email").value === "") {alert("L'email doit être rempli"); // On affiche un message
+    }else {
+        letquestion = "Souhaitez-vous réellement utiliserl'adresse suivante : "+ window.document.querySelector("#i_email").value;
+        if (confirm(question)) {window.document.querySelector("#form_contact").submit();// OK envoyer
+             }
+        }
+    };
