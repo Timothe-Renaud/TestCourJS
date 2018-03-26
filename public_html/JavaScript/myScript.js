703e84c6e36593adc1328a31b975f4721df1c285
timothe <timothe@tim>
2018-03-26 15:19:31 +0200

    first commit

---
+++ public_html/JavaScript/myScript.js
@@ -0,0 +1,13 @@
+function validEnvoi() {
+    if (
+            window.document.querySelector("#i_nom").value==="" &&window.document.querySelector("#i_prenom").value==="") {
+            alert("Le nom ou le prénom doivent être remplis");// On affiche un message
+        }
+        else {
+            window.document.querySelector("#form_contact").submit();// On peut envoyer
+        }
+    }
+    window.addEventListener("load", function() {
+        window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
+    }
+);
